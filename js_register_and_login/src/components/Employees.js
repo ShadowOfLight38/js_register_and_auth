66f629e048b7db62b15307aa4e40ba5308ac702f
Dmitrii Mishakov <gm.kaminari@gmail.com>
2023-05-29 14:33:07 +0300

    Added some basic functionality

---
+++ js_register_and_login/src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Dmitrii Mishakov",
+        Age:"33"
+    },
+    {
+        id:"2",
+        Name:"Liubov Mishakova",
+        Age:"33"
+    },
+    {
+        id:"3",
+        Name:"Random Memberie",
+        Age:"20"
+    }
+]
+
+export default Employees;
